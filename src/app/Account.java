1e60d912c1679329b776df6e5078b0e2a1f6359c
Dylan <devybuchi@gmail.com>
2019-10-18 23:17:13 -0300

    updated Account class

--- src/app/Account.java
+++ src/app/Account.java
@@ -8,19 +8,37 @@ public class Account implements IBaseRate {
     String name;
     String securityNumber;
     String accountNumber;
+    static int uniqueDigitNumbers = 73450;
 
     double rate;
     double balance;
 
     /**
-     * 
+     * constructor
      */
     public Account(String name, String securityNumber, double balance) {
+        // unique number +1 for every new account;
+        uniqueDigitNumbers++;
+
         this.name = name;
         this.securityNumber = securityNumber;
         this.balance = balance;
+        this.accountNumber = setAccountNumber();
 
         System.out.println(name + " " + securityNumber + " " + "$" + balance);
+        System.out.println("Account Number: " + this.accountNumber);
+    }
+
+    // set digital account number
+    public String setAccountNumber() {
+
+        String last2SecurityNumbers = this.securityNumber.substring(this.securityNumber.length() - 2,
+                this.securityNumber.length());
+
+        int random3Numbers = (int) (Math.random() * Math.pow(10, 3));
+
+        return last2SecurityNumbers + uniqueDigitNumbers + random3Numbers + "0";
+
     }
 
 }
